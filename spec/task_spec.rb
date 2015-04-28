a7b2504732014e3512a1cf780cc820421504550f
james robert williams & Adam McFadden <student@epicodus.com>
2015-04-28 09:27:31 -0700

    wrote second spec and passed to show empty array when no tasks are present

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -8,4 +8,16 @@ describe(Task) do
       expect(test_task.description()).to(eq("scrub the zebra"))
     end
   end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_task = Task.new("wash the lion")
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
 end
