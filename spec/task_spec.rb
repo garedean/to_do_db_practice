672878dd0073385eae51eeaaded3214eb93a5252
james robert williams & Adam McFadden <student@epicodus.com>
2015-04-28 09:41:05 -0700

    update Gemfile and run bundle

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -2,6 +2,10 @@ require 'rspec'
 require 'task'
 
 describe(Task) do
+  before() do
+    Task.clear()
+  end
+
   describe("#description") do
     it("lets you give a describtion") do
       test_task = Task.new("scrub the zebra")
@@ -14,7 +18,7 @@ describe(Task) do
       expect(Task.all()).to(eq([]))
     end
   end
-
+  
   describe("#save") do
     it("adds a task to the array of saved tasks") do
       test_task = Task.new("wash the lion")
