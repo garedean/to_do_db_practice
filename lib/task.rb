afb82c5b1e1dc873a9e03170d797d4302eee809c
james robert williams & alex caste <student@epicodus.com>
2015-05-04 12:18:19 -0700

    Update spec file.  Update ruby logic.  Change save, all method for SQL.

--- lib/task.rb
+++ lib/task.rb
@@ -1,20 +1,28 @@
 class Task
+  attr_reader(:description)
+
   @@all_tasks = []
 
-  define_method(:initialize) do |description|
-    @description = description
+  define_method(:initialize) do |attributes|
+    @description = attributes.fetch(:description)
   end
 
-  define_method(:description) do
-    @description
+  define_method(:==) do |another_task|
+    self.description().==(another_task.description())
   end
 
   define_method(:save) do
-    @@all_tasks.push(self)
+    DB.exec("INSERT INTO tasks (description) VALUES ('#{@description}');")
   end
 
   define_singleton_method(:all) do
-    @@all_tasks
+    returned_tasks = DB.exec("SELECT * FROM tasks;")
+    tasks = []
+    returned_tasks.each() do |task|
+      description = task.fetch("description")
+      tasks.push(Task.new(description: description))
+    end
+  tasks
   end
 
   define_singleton_method(:clear) do
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,37 +1,28 @@
-require 'rspec'
-require 'task'
+require 'spec_helper'
 
 describe(Task) do
-  before() do
-    Task.clear()
-  end
-
-  describe("#description") do
-    it("lets you give a describtion") do
-      test_task = Task.new("scrub the zebra")
-      expect(test_task.description()).to(eq("scrub the zebra"))
-    end
-  end
 
   describe(".all") do
     it("is empty at first") do
       expect(Task.all()).to(eq([]))
     end
   end
-  
+
+  describe("#==") do
+    it("is the same task if it has the scame description") do
+      task1 = Task.new(description: "learn this")
+      task2 = Task.new(description: "learn this")
+      expect(task1).to(eq(task2))
+    end
+  end
+
+
   describe("#save") do
     it("adds a task to the array of saved tasks") do
-      test_task = Task.new("wash the lion")
+      test_task = Task.new(description: "wash the lion")
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
   end
 
-  describe(".clear") do
-    it("empties out all of the saved tasks") do
-      Task.new("wash the lion").save()
-      Task.clear()
-      expect(Task.all()).to(eq([]))
-    end
-  end
 end
